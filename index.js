7f9fdeef8ab29d98b3831809e8049db68d8d847f
haoxin <coderhaoxin@outlook.com>
2013-03-29 13:40:14 +0800

    add test initialize files

--- index.js
+++ index.js
@@ -10,9 +10,47 @@ function OSSBucket (name, options) {
 util.inherits(OSSBucket, Resource);
 module.exports = OSSBucket;
 OSSBucket.label = "OSSBucket";
-OSSBucket.events = ['get', 'put'];
+OSSBucket.events = ['get', 'put', 'post', 'delete'];
+OSSBucket.basicDashboard = {
+  setting: [
+    {
+      name: 'bucket',
+      type: 'string'
+    }, {
+      name: 'key',
+      type: 'string'
+    }, {
+      name: 'secret',
+      type: 'string'
+    }
+  ]
+}
 
-OSSBucket.prototype.clientGeneration = true;
+OSSBucket.prototype.clientGeneration = false;
 OSSBucket.prototype.handle = function (context, next) {
-  console.log(context);
+  var request = context.req;
+  var bucket = this;
+  var domin = {
+    url: context.url
+  }
+
+  if (request.internal) {
+    if (request.method === 'PUT') {
+      console.log('oss put');
+    } else if (request.method === 'DELETE') {
+      console.log('oss delete');
+    } else if (request.method === 'POST') {
+      console.log('oss post');
+    } else if (request.method === 'GET') {
+      console.log('oss get');
+    } else {
+      console.log('oss unknow');
+    }
+  } else {
+    console.log('oss unvalid');
+  }
+}
+OSSBucket.prototype.get = function (context, next) {
+ var bucket = this;
+ console.log(bucket);
 }
