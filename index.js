679783f4a0bdfa47bcda144138b4da49549221ba
haoxin <coderhaoxin@outlook.com>
2013-03-29 10:57:00 +0800

    add basic code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var a = 0;
+var Resource = require('deployd/lib/resource');
+var Http = require('deployd/lib/util/http');
+var fs = require('fs');
+var util = require('util');
+var path = require('path');
+
+function OSSBucket (name, options) {
+  Resource.apply(this, arguments);
+}
+util.inherits(OSSBucket, Resource);
+module.exports = OSSBucket;
+OSSBucket.label = "OSSBucket";
+OSSBucket.events = ['get', 'put'];
+
+OSSBucket.prototype.clientGeneration = true;
+OSSBucket.prototype.handle = function (context, next) {
+  console.log(context);
+}
