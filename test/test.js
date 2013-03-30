1f62529f06d5bdba7f9bb05bfd7f5be301233419
haoxin <coderhaoxin@outlook.com>
2013-03-30 11:49:58 +0800

    initialize test script

--- test/test.js
+++ test/test.js
@@ -1,5 +1,32 @@
-describe('', function () {
-  it('', function () {
-    ;
+var request = require('request');
+
+var urlObject = {
+  protocol: 'http',
+  hostname: 'localhost',
+  port: '2403',
+  pathname: '/'
+}
+
+var url = require('url').format(urlObject);
+
+var options = {
+  method: 'POST',
+  json: false,
+  url: '',
+}
+
+describe('test dpd-oss\n', function () {
+  it('GET', function (done) {
+    console.log(url);
+    done();
+  })
+  it('POST', function (done) {
+    done();
+  })
+  it('DELETE', function (done) {
+    done();
+  })
+  it('PUT', function (done) {
+    done();
   })
 })
